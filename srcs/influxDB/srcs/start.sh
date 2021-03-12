f6d072bac3c6ed17cb1d90b18045cfb3ae0f4211
Gropopus <thomas.sembely@gmail.com>
2021-03-12 15:27:36 +0100

    vsftpd?

--- srcs/influxDB/srcs/start.sh
+++ srcs/influxDB/srcs/start.sh
@@ -1,4 +1,4 @@
 rc-service influxdb start
-echo "CREATE DATABASE metrics" | influx
+echo "CREATE DATABASE metrics-db" | influx
 echo "CREATE USER thsembel WITH PASSWORD thsembel WITH ALL PRIVILEGES" | influx
-sleep infinity
+telegraf & sleep infinity
